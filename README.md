cb9fc46fb7ceea7f16126d4c8a2fc3fa93ceb0c1
berke <berke.uysal@fau.de>
2024-06-26 15:51:06 +0200

    final commit and README update

--- README.md
+++ README.md
@@ -1,18 +1,119 @@
 # Repository for ss24.1.1/team776
+
 This is the repository for you solution. You can modify this README file any way you see fit.
 
 **Topic:** SS24 Assignment 1.1: Find Train Connections
 
-# Assignment 1.1: Find train connections
+# Train Route Optimization
 
-This repository contains the data for the assignment 1.1 of the AI1 systems project in summer semester 2024.
-The assignment sheet contains a detailed description of the data.
+## Dependencies
 
-#### `verify.py`
-You can use `verify.py` to check your solutions for the example problems.
-It will send them to a server and print out some feedback.
-It requires the `requests` package (`python3 -m pip install requests`).
-Then you can run the script with:
+This project is written in Python 3.11. While it may work on older versions, this has not been tested. The project
+relies on the following external libraries:
+
+- `heapq` for priority queue implementation.
+- `pandas` for data manipulation and CSV reading.
+- `networkx` for graph construction and manipulation.
+- `datetime` for time parsing and handling.
+- `matplotlib` for graph visualization.
+
+Install the necessary libraries using pip:
+
+```bash
+pip install pandas networkx matplotlib
 ```
-python3 verify.py path/to/your/example/solutions
-```
+
+## Repository Structure
+
+- `schedule.csv` and `mini-schedule.csv`: CSV files with the train schedule information.
+- `example-problems.csv` and `example-solutions.csv`: Example data files used for debugging and validation purposes.
+- `my-example-solutions.csv`: Output file storing the generated answers. This is used to compare the generated solutions
+  with the example solutions.
+- `problems.csv`: Contains the problem data that needs to be solved, consisting of 120 problems.
+- `solutions.csv`: Contains the generated answers for the problems.
+- `main.py`: The primary script that reads the problems, solves them, and writes the answers to a file.
+- `Graphs`: File with two plots of the graphs drawn from the schedules.
+
+## How to Run
+
+To run this code with other data, modify the paths and filenames in the `main.py` script accordingly. You can edit the
+loop at the bottom of the script to specify the dataset you want to process.
+
+## The Problem
+
+The task is to find optimal train routes between stations based on various cost functions. The routes are constructed
+from train schedules, and the cost can be defined in terms of the number of stops, travel time, price or arrival time.
+The problem dataset includes different schedules and cost functions to optimize for.
+
+### Problem Modes and Challenges
+
+The problem data is divided into four main tasks, each addressing a different challenge. Here's a brief overview:
+
+| Problem | Challenges   |
+|---------|--------------|
+| A       | stops        |
+| B       | travel time  |
+| C       | price        |
+| D       | arrival time |
+
+## My Approach
+
+Before focusing on specific challenges, I first created the skeleton of the solver code:
+
+1. **Read from CSV**: I started by reading the problem data and train schedules from CSV files using `pandas`.
+2. **Construct Graphs**: I constructed directed graphs using `networkx`, where nodes represent stations and edges
+   represent costs. Each edge carries relevant information such as travel time, distance, and train number.
+3. **Visualize the Graph**: Optionally, I checked the structure of the graph by visualizing it with `matplotlib` to
+   ensure correctness.
+4. **Optimal Path Finder**: I created a pathfinder function that takes the required information from the
+   problem statement and adjusts the weight function accordingly.
+5. **Dijkstra Function**: The pathfinder calls the Dijkstra function, implemented to handle different cost functions.
+6. **Format and Write Results**: After finding the path and costs, the results are formatted as required and written to
+   a CSV file.
+
+### Problem A
+
+This problem asks to find the path with the fewest stops. Each edge's cost is set to 1 since each node (station) is
+equally weighted in terms of stops.
+
+### Problem B
+
+For the travel time, the cost is the time it takes to travel from one station to another. The Dijkstra function handles
+this by using travel time as the edge weight.
+
+### Problem C
+
+This problem is more complex. It requires tracking the price with the following condition: from the time of boarding a
+train, the cost function increases by 1 until the 10th station, after which it stops increasing. This simulates
+switching from a stop-based ticket to a train-based ticket.
+
+### Problem D
+
+This is the most challenging part of the homework. The goal is to minimize the arrival time. The cost is set to the
+arrival time, and Dijkstra's algorithm tries to find the earliest arrival. This part still has issues and is not fully
+implemented. The challenge lies in accurately adjusting for overnight travels and ensuring the correct sequence of train
+departures and arrivals.
+
+
+## Code Breakdown
+
+### Main Function
+
+The `main` function orchestrates the workflow:
+
+- Reads the problem data.
+- Constructs the train route graphs.
+- Optionally visualizes the graphs.
+- Applies the optimal pathfinding algorithm for each problem.
+- Saves the solutions to a CSV file.
+
+### Functions
+
+- `parse_time`: Parses time strings into `datetime` objects.
+- `construct_graph`: Builds the graph from train schedule CSV files.
+- `visualize_graph`: Visualizes the graph using `matplotlib`.
+- `format_datetime`: Formats `datetime` objects for output.
+- `dijkstra`: Implements Dijkstra's algorithm to find the shortest path in the graph.
+- `path_writer`: Converts the path found by Dijkstra's algorithm into a readable format.
+- `optimal_path_finder`: Finds the optimal path for a given problem row.
+- `format_solution`: Formats the solution for CSV output.
--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import heapq
-
 import pandas as pd
 import networkx as nx
-from datetime import datetime
+from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 
-PROBLEMS = "example-problems.csv"
+PROBLEMS = "problems.csv"
 mini_schedule_graph = None
 schedule_graph = None
 
 
-def parse_time(time_str):
-    time_str = time_str.strip("'")
-    return datetime.strptime(time_str, "%H:%M:%S")
+# Function to parse time strings into datetime objects
+def parse_time(time_str, default_year=1970):
+    time_str = time_str.strip("'")  # Remove any single quotes around the time string
+    try:
+        # Try to parse the full datetime string (including the year)
+        return datetime.strptime(time_str, "%Y-%m-%d %H:%M:%S")
+    except ValueError:
+        # If year is missing, assume the default year
+        try:
+            time_with_default_year = f"{default_year}-01-01 {time_str}"
+            return datetime.strptime(time_with_default_year, "%Y-%m-%d %H:%M:%S")
+        except ValueError:
+            raise ValueError("Invalid time format. Expected 'HH:MM:SS' or 'YYYY-MM-DD HH:MM:SS'")
 
 
+# Function to construct a graph from a CSV file
 def construct_graph(file_path):
     graph = nx.MultiDiGraph()
     previous_node = None
@@ -36,15 +46,15 @@ def construct_graph(file_path):
                 "station_code": station_code,
             }
 
+            # Add or update the node with train information
             if station_code in graph:
                 graph.nodes[station_code]['trains'].append(train_info)
             else:
-                # Add the node with the train information list
                 graph.add_node(station_code, trains=[train_info])
 
+            # Add edges between consecutive stations of the same train
             if previous_node:
                 if train_no == previous_node["train_no"]:
-
                     traveltime = (arrival_time - previous_node["departure_time"]).total_seconds()
                     if traveltime < 0:  # handle next day arrival
                         traveltime += 24 * 3600
@@ -53,43 +63,64 @@ def construct_graph(file_path):
                                    station_code,
                                    stops=1,
                                    traveltime=int(traveltime),
+                                   price=1,
                                    train_info=previous_node)
 
             previous_node = train_info
     return graph
 
 
+# Function to visualize the graph
 def visualize_graph(graph):
     print("visualizing")
-    pos = nx.spring_layout(graph, k=20)
-    plt.figure(figsize=(12, 8))
+    pos = nx.kamada_kawai_layout(graph)
+    plt.figure(figsize=(25, 25))
     nx.draw(graph, pos, with_labels=True, node_size=500, node_color='lightblue', font_size=10, font_weight='bold',
             edge_color='gray')
-    labels = nx.get_edge_attributes(graph, 'weight')
+    labels = nx.get_edge_attributes(graph, 'price')
     nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels, font_color='red')
     plt.title('Train Connection Graph')
     plt.show()
 
 
-def dijkstra(graph, start, end, weight='weight'):
+# Function to format a datetime object into a specific string format
+def format_datetime(dt):
+    base_date = datetime(1970, 1, 1)
+    delta = dt - base_date
+    days = delta.days
+    hours, remainder = divmod(delta.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    formatted_time = f"{days:02}:{hours:02}:{minutes:02}:{seconds:02}"
+    return formatted_time
+
+
+# Dijkstra's algorithm to find the shortest path
+def dijkstra(graph, start, end, weight):
     # Clear visited tags before running the algorithm
     for u, v, key in graph.edges(keys=True):
         if 'visited' in graph[u][v][key]:
             del graph[u][v][key]['visited']
 
+    # Parse the departure time
+    if 'arrivaltime' in weight:
+        departure_time = datetime.strptime(weight.split(" ")[1], "%H:%M:%S")
+    else:
+        departure_time = None
+
     # Priority queue to hold nodes to explore
     queue = []
-    # Start with the start node with a cost of 0
-    heapq.heappush(queue, (0, start, None))
+    heapq.heappush(queue, (0, start, None, (None, 0), departure_time))
+
     # Dictionary to hold the cost to reach each node
     costs = {node: float('inf') for node in graph.nodes}
     costs[start] = 0
+
     # Dictionary to hold the path to each node
     predecessors = {node: None for node in graph.nodes}
 
     while queue:
-        # Get the node with the smallest cost
-        current_cost, current_node, edge = heapq.heappop(queue)
+        current_cost, current_node, current_edge, prev_train, current_time = heapq.heappop(queue)
 
         # If we reached the end node, we're done
         if current_node == end:
@@ -98,38 +129,78 @@ def dijkstra(graph, start, end, weight='weight'):
         # Explore the neighbors
         for neighbor in graph.successors(current_node):
             for key, data in graph[current_node][neighbor].items():
-                weight_value = data.get(weight, 1)  # Default weight is 1 if not provided
-                new_cost = current_cost + weight_value
+                train_info = data["train_info"]
+                train_no = train_info['train_no']
+                train_departure_time = train_info['departure_time']
+
+                if 'arrivaltime' in weight:
+                    if train_departure_time < current_time:
+                        train_departure_time += timedelta(days=1)  # Adjust for overnight travel
+
+                    travel_duration = data['traveltime']
+                    new_time = train_departure_time + timedelta(seconds=travel_duration)
+                    # Check if changing trains requires additional time (e.g., 15 minutes)
+                    if current_edge and current_edge[2] != key:
+                        train_change_duration = timedelta(minutes=15)
+                        new_time += train_change_duration
+                else:
+                    new_time = current_time
+
+                # Calculate the cost based on the weight function
+                if weight == 'price':
+                    weight_value = data.get('price', 1)
+                    if prev_train[0] is not None:
+                        if train_no == prev_train[0]:
+                            if prev_train[1] >= 10:
+                                new_cost = current_cost  # No additional cost after 10th stop
+                            else:
+                                new_cost = current_cost + weight_value
+
+                            prev_train = (train_no, prev_train[1] + 1)  # Increment stop count
+                        else:
+                            new_cost = current_cost + weight_value
+                            prev_train = (train_no, 1)  # Reset stop count for new train
+                    else:
+                        new_cost = current_cost + weight_value
+                        prev_train = (train_no, 1)
+                elif 'arrivaltime' in weight:
+                    new_cost = new_time.timestamp()
+                else:
+                    weight_value = data.get(weight, 1)
+                    new_cost = current_cost + weight_value
 
                 # If we found a cheaper path to the neighbor, update its cost and path
                 if new_cost < costs[neighbor]:
                     costs[neighbor] = new_cost
                     predecessors[neighbor] = (current_node, key)
-                    heapq.heappush(queue, (new_cost, neighbor, (current_node, neighbor, key)))
+                    heapq.heappush(queue, (new_cost, neighbor, (current_node, neighbor, key), prev_train, new_time))
 
     # Reconstruct the path from end to start using the predecessors dictionary
     path = []
     current = end
-    while predecessors[current] is not None:
+    i = 0
+    while predecessors[current] is not None and i < 100:
         prev, edge_key = predecessors[current]
         path.insert(0, (prev, current, edge_key))
         current = prev
+        i += 1
 
     # Tag the edges as visited
     for u, v, key in path:
-        if 'visited' not in graph[u][v][key]:
-            graph[u][v][key]['visited'] = True
+        graph[u][v][key]['visited'] = True
+    if 'arrivaltime' in weight:
+        return path, format_datetime(datetime.fromtimestamp(costs[end]))
+    else:
+        return path, costs[end]
 
-    return path, costs[end]
 
-
+# Function to write the path into a readable format
 def path_writer(path, graph):
     edges = []
 
     for u, v, key in path:
         if 'visited' in graph[u][v][key]:
             edges.append((u, v, key, graph[u][v][key]))
-    print(edges)
 
     sol = ""
     prev_edge = None
@@ -141,21 +212,20 @@ def path_writer(path, graph):
                          f'{stop[3]["train_info"]["islno"]}')
         prev_edge = stop[3]
     sol = sol + f' -> {edges[-1][3]["train_info"]["islno"] + 1}'
-    print(sol)
 
     return sol
 
 
+# Function to find the optimal path for a given row in the problems CSV
 def optimal_path_finder(row):
     global mini_schedule_graph
     global schedule_graph
-    print(row)
+
     from_station = row["FromStation"]
     to_station = row["ToStation"]
     cost_function = row["CostFunction"]
 
     graph = mini_schedule_graph if row["Schedule"] == "mini-schedule.csv" else schedule_graph
-
     if cost_function == "stops":
         weight = 'stops'
     elif cost_function == "traveltime":
@@ -163,7 +233,7 @@ def optimal_path_finder(row):
     elif cost_function == "price":
         weight = 'price'
     else:
-        weight = 'arrivaltime'
+        weight = cost_function
 
     path, cost = dijkstra(graph, from_station, to_station, weight=weight)
 
@@ -174,6 +244,7 @@ def optimal_path_finder(row):
     }
 
 
+# Function to format the solution for output
 def format_solution(solution):
     problem_no = solution['ProblemNo']
     connections = solution['Connection']
@@ -182,6 +253,7 @@ def format_solution(solution):
     return problem_no, connections_str, cost
 
 
+# Main function to execute the workflow
 def main():
     global mini_schedule_graph
     global schedule_graph
@@ -194,19 +266,15 @@ def main():
     # Visualize the Graphs
     # visualize_graph(mini_schedule_graph)
     # visualize_graph(schedule_graph)
-    # print("All stations and their train information:")
-    # for node, data in mini_schedule_graph.nodes(data=True):
-    #     print(f"Station: {node}, Attributes: {data}")
 
     # Get optimal path
-    # solutions = problem_df.apply(optimal_path_finder, axis=1)
-
-    solutions = problem_df.iloc[:40].apply(optimal_path_finder, axis=1)
+    solutions = problem_df.apply(optimal_path_finder, axis=1)
+    # solutions = problem_df.iloc[61:62].apply(optimal_path_finder, axis=1)
 
     # Save solutions to CSV
     formatted_solutions = [format_solution(sol) for sol in solutions]
     solutions_df = pd.DataFrame(formatted_solutions, columns=['ProblemNo', 'Connection', 'Cost'])
-    solutions_df.to_csv("my-example-solutions.csv", index=False)
+    solutions_df.to_csv("my-solutions.csv", index=False)
 
 
 if __name__ == "__main__":
